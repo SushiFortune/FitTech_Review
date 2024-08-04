0425c69499135623b184d3777f19bd31ba4b9baa
SushiFortune <r_maouko@live.concordia.ca>
2024-08-03 20:34:37 -0700

    filtered out non related fitbit sense 2 products

--- src/data_collection.py
+++ src/data_collection.py
@@ -5,21 +5,16 @@ def search_fitbit_sense_2():
     url = "https://api.bestbuy.com/v1/products(search=fitbit sense 2)?format=json"
     params = {
         'apiKey': api_key,
-        'show': 'sku,name,salePrice,customerReviewAverage'
+        'show': 'sku,name,salePrice,customerReviewAverage',
+     
     }
     response = requests.get(url, params=params)
     if response.status_code == 200:
         data = response.json()
-        return data['products']
+        sense_2_products = [product for product in data['products'] if 'Sense 2' in product['name']]
+        return sense_2_products
     else:
         return None
-
-products = search_fitbit_sense_2()
-if products:
-    for product in products:
-        print(f"Name: {product['name']}, Rating: {product.get('customerReviewAverage', 'No rating available')}")
-else:
-    print("No products found.")
 
 
     
