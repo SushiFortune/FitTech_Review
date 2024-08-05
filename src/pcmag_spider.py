f62a1ec08ae2db64d7a73206a8b039a6a21ca0e6
SushiFortune <r_maouko@live.concordia.ca>
2024-08-05 16:29:18 -0700

    implementing webscraping using selenium to retrieve review score from PCMAG.

--- src/pcmag_spider.py
+++ src/pcmag_spider.py
@@ -1,13 +1,16 @@
-import scrapy
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.edge.service import Service
+from webdriver_manager.microsoft import EdgeChromiumDriverManager
 
-class PcmagSpider(scrapy.Spider):
-    name = 'pcmag_spider'
-    allowed_domains = ['pcmag.com']
-    start_urls = ['https://www.pcmag.com/reviews/fitbit-sense-2']
+service = Service(EdgeChromiumDriverManager().install())
+driver = webdriver.Edge(service=service)
+driver.get('https://www.pcmag.com/reviews/fitbit-sense-2')
 
-    def parse(self, response):
-        review_number = response.css('.review-count::text').get()
-        yield {
-            'review_number': review_number
-        }
+rating_element = driver.find_element(By.XPATH, '/html/body/div[3]/div/main/div/header/div/div[1]/div[2]')
+rating = rating_element.text
+print(f'Review Rating: {rating}')
+
+driver.quit()
+
 
