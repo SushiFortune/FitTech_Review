5f8a92d3f8a086cb1a1dd69438140c8ddedcff2d
SushiFortune <r_maouko@live.concordia.ca>
2024-08-10 12:53:09 -0700

    Refactoring UI_Fortmatting.py

--- src/UI_formatting.py
+++ src/UI_formatting.py
@@ -1,27 +1,34 @@
 
 def format(user_input,id):
     user_input = user_input.strip().lower()
-    
-    if id==0:
-        if " - " in user_input:
-            parts = user_input.split(" - ")
-            if len(parts) == 2 and all(parts):
-                return user_input
-    
-        words = user_input.split()
-        if len(words) > 1 and "-" not in user_input:
+    if user_input.isalpha()!= True:
+        return None 
+        
+    else : 
+        words=user_input.split()
+
+        # Formatting for BestBuy 
+        if id==0: 
+            # if " - " in user_input:
+            #     parts = user_input.split(" - ")
+            #     if len(parts) == 2 and all(parts):
+            #         return user_input
+        
+            # words = user_input.split()
+            # if len(words) > 1 and "-" not in user_input:
             formatted_UI = words[0] + " - " + " ".join(words[1:])
             return formatted_UI
+            
+            # return None
+
+        # Formatting for PCMag
+        elif id==1:
+
+            # words = user_input.split()
+            s="-"
+            # if len(words) > 1 and "-" not in user_input:
+            formatted_UI = s.join(words)
+            return formatted_UI
+        
         
         return None
-    
-    elif id==1:
-
-        words = user_input.split()
-        s="-"
-        if len(words) > 1 and "-" not in user_input:
-            formatted_UI = s.join(words)
-        return formatted_UI
-    
-    
-    return None
