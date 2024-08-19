0e888bb04039c3ae681de483adaf1811c71dc62b
SushiFortune <r_maouko@live.concordia.ca>
2024-08-18 20:41:43 -0700

    fixed URL formatting for API response

--- src/testBB.py
+++ src/testBB.py
@@ -1,34 +1,38 @@
 import requests
 from config import api_key
-from urllib.parse import quote
+
 
 def fetch_product_data(model_name):
-
-    
+    model_name='garmin-fenix-7s-gps-smartwatch-42-mm-fiber-reinforced-polymer-silver'
     url = f"https://api.bestbuy.com/v1/products(search={model_name})?format=json"
+    
     params = {
         'apiKey': api_key,
         'show': 'sku,name,salePrice,customerReviewAverage',
     }
     response = requests.get(url, params=params)
+
     if response.status_code == 200:
         return response.json()
     else:
         response.raise_for_status()
 
-
 def filter_data(model_name, data):
     keywords = ["smartwatch", "tracker"]
     filtered_products_v1 = [product for product in data['products'] if model_name.lower() in product['name'].lower()]
-    filtered_products_v2 = [product for product in data['products'] if any(keyword.lower() in product['name'].lower() for keyword in keywords)]
-    return filtered_products_v2
-
+    # filtered_products_v2 = [product for product in filtered_products_v1 if any(keyword.lower() in product['name'].lower() for keyword in keywords)]
+    return filtered_products_v1
 
 try:
-   model_name = "fitbit inspire 2"
-   data= fetch_product_data(model_name)
-   products=filter_data(model_name, data)
-   print(products)
+    model_name = 'Garmin - fēnix 7S'
+    data = fetch_product_data(model_name)
+    products=filter_data(model_name,data)
+    print(products)
 
 except requests.exceptions.RequestException as e:
     print(f"An error occurred: {e}")
+
+
+
+
+
