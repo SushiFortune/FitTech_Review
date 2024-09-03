ae0bbac49000d8cb58db5298ad4596735c05487f
SushiFortune <r_maouko@live.concordia.ca>
2024-09-03 14:20:07 -0700

    refactoring

--- src/bestbuy_brand_processing.py
+++ src/bestbuy_brand_processing.py
@@ -5,10 +5,10 @@ def choice(user_input):
     user_input=user_input.lower()
 
     if "garmin" in user_input:
-        return bestbuy_fitbit_data_collection.get_rating(user_input)
+        return bestbuy_garmin_data_collection.get_rating(user_input)
     
     elif "fitbit" in user_input:
-        return bestbuy_garmin_data_collection.get_rating(user_input)
+        return bestbuy_fitbit_data_collection.get_rating(user_input)
     
     else:
         return None
